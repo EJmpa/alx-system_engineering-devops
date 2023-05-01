f06f85b8574c9d91e6bf5ba883c6373e1e06e822
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-05-01 16:14:58 -0700

    Zombie

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -12,20 +12,19 @@ int infinite_while(void);
  */
 int main(void)
 {
-    pid_t pid;
-    int i;
+	pid_t pid;
+	int i;
 
-    for (i = 0; i < 5; i++)
-    {
-        pid = fork();
-        if (pid > 0)
-            printf("Zombie process created, PID: %d\n", pid);
-        else
-            exit(0);
-    }
-    infinite_while();
-
-    return (0);
+	for (i = 0; i < 5; i++)
+	{
+		pid = fork();
+		if (pid > 0)
+			printf("Zombie process created, PID: %d\n", pid);
+		else
+			exit(0);
+	}
+	infinite_while();
+	return (0);
 }
 
 /**
@@ -33,11 +32,12 @@ int main(void)
  *
  * Return: Always 0
  */
+
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
