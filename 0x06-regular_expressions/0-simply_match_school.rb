84530cc6d5cbb51102cd1ef23e823ed618bba2d2
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-05-04 10:44:01 -0700

    Task complete

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,9 @@
 #!/usr/bin/env ruby
+# coding: utf-8
 # Ruby script that accepts one argument and pass it to a regular expression matching method
 puts ARGV[0].scan(/School/).join
+
+# This is a Ruby script that uses a regular expression to match the word ‘School’ in the first
+# argument passed to the script. The `scan` method returns an array of all occurrences of the regular
+# expression in the input string. The `join` method then concatenates the elements of the array into
+# a single string, which is printed to the console using `puts`.
