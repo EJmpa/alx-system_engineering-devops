7c52f3aad709e1d4c52713c892f1e58d249c2169
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-06-26 17:14:39 +0100

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,6 @@
 # puppet script - configure ssh authentication:
 
 file_line { 'Turn off passwd auth':
-<<<<<<< HEAD
-  ensure => 'present',
-  path   => '/etc/ssh/config',
-  line   => 'PasswordAuthentication no',
-}
-
-file_line { 'Declare identity file':
-  ensure => 'present',
-  path   => '/etc/ssh/config',
-  line   => 'IdentityFile ~/.ssh/school',
-=======
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
 }
@@ -24,5 +13,4 @@ file_line { 'Declare identity file':
 service { 'ssh':
   ensure => running,
   enable => true,
->>>>>>> 60b42c05b8f0c47ba1c08d25d5604d77a7d0a2ec
 }
