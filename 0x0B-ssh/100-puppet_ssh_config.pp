b5bcc638cfeaf810c114efe5093d7851496e191a
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-06-26 09:11:49 -0700

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Create ssh congig file using puppet
 file_line { 'Turn off passwd auth':
   ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
+  path   => '/etc/ssh/config',
   line   => 'PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
   ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
+  path   => '/etc/ssh/config',
   line   => 'IdentityFile ~/.ssh/school',
 }
