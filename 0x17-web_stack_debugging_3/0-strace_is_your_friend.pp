5acc314d4575df004716cdb4ba58530ecd07a9d4
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-08-11 18:20:01 +0100

    Update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,35 +1,10 @@
-# Install required packages (if not already installed)
-package { 'apache2':
-  ensure => 'installed',
-}
+# A puppet code to replace a line in a file on a server
 
-# Ensure the configuration directory exists
-file { '/etc/apache2/conf.d':
-  ensure => 'directory',
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0755',
-}
+$file_to_edit = '/var/www/html/wp-settings.php'
 
-# Add the missing configuration file for custom PHP module
-file { '/etc/apache2/conf.d/mod_custom_php.conf':
-  ensure  => 'present',
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => "# Load custom PHP module\nLoadModule custom_php_module /usr/lib/apache2/modules/mod_custom_php.so\n\n<IfModule custom_php_module>\n,
-  require => File['/etc/apache2/conf.d'],
-}
+#replace line containing "phpp" with "php"
 
-# Stop the Apache service (if running)
-service { 'apache2':
-  ensure => 'stopped',
-  enable => false,
-}
-
-# Start the Apache service
-service { 'apache2':
-  ensure => 'running',
-  enable => true,
-  require => File['/etc/apache2/conf.d/mod_custom_php.conf'],
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
